cd8d4794061cc9dae5f59bed0312b9eab3997ac6
JK-8989 <jiyoungkim365@gmail.com>
2022-05-10 23:33:18 -0700

    Edit color experiment

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const text = "Typing Effect with Cursor"
-// const content = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum reprehenderit quia quas veritatis exercitationem beatae iure molestias obcaecati esse. Recusandae alias, maxime reprehenderit unde, earum quibusdam ex adipisci sint magnam, ullam minus. Repellat ad maxime nostrum veniam quia, tempore aut vel, possimus repudiandae fugiat consectetur iure accusantium magni error odit a magnam laboriosam perspiciatis dolor tenetur consequatur dolorem sequi?'
 const heading = document.querySelector('.heading')
+const body = document.querySelector('body')
+heading.style.color = 'orange'
+body.style.background = 'whitesmoke'
+// const content = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum reprehenderit quia quas veritatis exercitationem beatae iure molestias obcaecati esse. Recusandae alias, maxime reprehenderit unde, earum quibusdam ex adipisci sint magnam, ullam minus. Repellat ad maxime nostrum veniam quia, tempore aut vel, possimus repudiandae fugiat consectetur iure accusantium magni error odit a magnam laboriosam perspiciatis dolor tenetur consequatur dolorem sequi?'
+
 // const Lorem = document.querySelector('.lorem')
 
 let index = 0
